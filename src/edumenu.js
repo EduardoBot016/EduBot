f77e21a2d37b56bde4dd9a6caf34faad18bb4de3
EduardoBot016 <79762266+EduardoBot016@users.noreply.github.com>
2021-03-08 12:45:27 -0400

    Add files via upload

---
+++ src/edumenu.js
@@ -0,0 +1,12 @@
+const edumenu = (prefix, pushname) => {
+    return `◪ *Comandos do Edubot*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.edumenu = edumenu
---
+++ src/help.js
@@ -0,0 +1,64 @@
+const help = (prefix) => {
+	return `
+「 *🇪​🇩​🇺​🇦​🇷​🇩​🇴​ 🇧​🇴​🇹​* 」
+
+◪ *Informações*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Criador : Eduardo
+
+╭══•›「 *Informações Básicas do Programador* 」
+│Nome: Eduardo Fagundes
+│Telefone: (69) 99982-3806
+│Whats: wa.me/5569999823806
+│Instagram: https://bit.ly/2NOHxJK
+╰══════════════
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}infodonumero
+  ├─ ❏ ${prefix}listadebloqueio
+  ├─ ❏ ${prefix}listasdebatepapo
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}relatoriosdeerro
+◪ *COMANDO O DONO*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+◪ *FAZER*
+  │
+  ├─ ❏ ${prefix}figurinha
+  ├─ ❏ ${prefix}figurinhagif
+  ├─ ❏ ${prefix}paraimg
+  ├─ ❏ ${prefix}paramp3
+
+◪ *DOWNLOADER*
+  │
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+◪ *SOM*
+  │
+  ├─ ❏ ${prefix}play
+  └─ ❏ ${prefix}tts
+◪ *MÚSICA*
+  │
+  └─ ❏ ${prefix}lirik
+◪ *OUTROS*
+  │
+  ├─ ❏ ${prefix}send
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex
+  ├─ ❏ ${prefix}exe
+  ├─ ❏ ${prefix}qrcode
+  ├─ ❏ ${prefix}afk
+  ├─ ❏ ${prefix}timer
+  ├─ ❏ ${prefix}fml
+  └─ ❏ ${prefix}fml2
+`
+}
+
+exports.help = help
